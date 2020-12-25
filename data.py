913e70d26e0c22bc39a0366e4660b36a8404a515
smazydev <alizafar1437@gmail.com>
2020-12-25 13:44:59 +0500

    full quiz app

---
+++ data.py
@@ -0,0 +1,8 @@
+question_data = [
+    {"text": "A slug's blood is green.","answer":"True"},
+    {"text": "The loudest animal is the African Elephant","answer":"False"},
+    {"text": "Approximately one quarter of human bones are in the feet.","answer":"True"},
+    {"text": "The total surface area of a human lungs is the size of a football pitch","answer":"True"},
+    {"text": "It is illegal to pee in the ocean in Portugal","answer":"True"},
+    {"text": "You can lead a cow downstairs but not upstairs","answer":"False"}
+] 
